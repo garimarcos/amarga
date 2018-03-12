1c66508566dbace200ee7e979268e0481b7ea515
garimarcos <garikoitzmarcos@gmail.com>
2018-03-12 21:53:48 +0100

    Krokodiloa sortuta

---
+++ src/packbarbestial/Krokodiloa.java
@@ -0,0 +1,10 @@
+package packbarbestial;
+
+public class Krokodiloa extends Karta {
+
+	public Krokodiloa(int pIndarra, boolean pErrekurrente) {
+		super(pIndarra, pErrekurrente);
+		// TODO Auto-generated constructor stub
+	}
+
+}
