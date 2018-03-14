0484fbdba42de19571c0ced4320152a168375dc2
garimarcos <garikoitzmarcos@gmail.com>
2018-03-14 12:33:11 +0100

    Sugea gehituta

---
+++ src/packbarbestial/Sugea.java
@@ -0,0 +1,10 @@
+package packbarbestial;
+
+public class Sugea extends Karta {
+
+	public Sugea(int pIndarra, boolean pErrekurrente) {
+		super(9, false);
+		// TODO Auto-generated constructor stub
+	}
+
+}
