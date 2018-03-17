0414e1cc402c6eaa592a86ae53c5c032389fbafa
garimarcos <garikoitzmarcos@gmail.com>
2018-03-17 19:52:59 +0100

    Salbuespenak kudeatuta

---
+++ src/packbarbestial/KopuruEzEgokiaException.java
@@ -0,0 +1,30 @@
+package packbarbestial;
+
+public class KopuruEzEgokiaException extends Exception {
+
+	public KopuruEzEgokiaException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public KopuruEzEgokiaException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public KopuruEzEgokiaException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public KopuruEzEgokiaException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public KopuruEzEgokiaException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
--- src/packbarbestial/Mahaia.java
+++ src/packbarbestial/Mahaia.java
@@ -1,5 +1,6 @@
 package packbarbestial;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Mahaia {
@@ -19,19 +20,33 @@ public class Mahaia {
 	
 	private void sortuJokalariak(){
 		Scanner sc=new Scanner(System.in);
+		boolean ondo=false;
 		System.out.println("Sartu jokalari kopurua (ordenagailuaren kontra jokatzetakoan, sartu 1): ");
-		int kop=sc.nextInt();
-		for(int i=1;i<=kop;i++){ //sartutako kopuruaren arabera burutuko dugu metodo hau, horrela hedagarria izango da jokalari gehiago sartzerako orduan
-			Scanner sc1=new Scanner(System.in);
-			System.out.println("Sartu jokalariaren izena: ");
-			String izena=sc1.nextLine();
-			Scanner sc2=new Scanner(System.in);
-			System.out.println(izena + ", sartu nahi duzun kolorea");
-			String kolorea=sc2.nextLine();
-			ListaJokalariak.getNireLista().add(new Jokalaria(izena,kolorea)); //EMAn sartu
-			sc1.close();
-			sc2.close();
-		}
+		do{
+			try{
+				int kop=sc.nextInt();
+				if(kop!=1){
+					System.out.println("Momentuz jokalari bakarrarekin jolasteko aukera dago, berriz saiatu zaitez");
+					throw new KopuruEzEgokiaException();
+				}
+				for(int i=1;i<=kop;i++){ //sartutako kopuruaren arabera burutuko dugu metodo hau, horrela hedagarria izango da jokalari gehiago sartzerako orduan
+					Scanner sc1=new Scanner(System.in);
+					System.out.println("Sartu jokalariaren izena: ");
+					String izena=sc1.nextLine();
+					Scanner sc2=new Scanner(System.in);
+					System.out.println(izena + ", sartu nahi duzun kolorea");
+					String kolorea=sc2.nextLine();
+					ListaJokalariak.getNireLista().add(new Jokalaria(izena,kolorea)); //EMAn sartu
+					sc1.close();
+					sc2.close();
+					ondo=true;
+				}
+			}catch(InputMismatchException e){
+				e.printStackTrace();
+			}catch(KopuruEzEgokiaException e){
+				e.printStackTrace();
+			}
+		}while(!ondo);
 		sc.close();
 	}
 	
